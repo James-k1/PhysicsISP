f864fdce95899e49c64c2ae78bee81f6c346c87f
BuildTools <unconfigured@null.spigotmc.org>
2023-03-23 02:58:56 -0400

    i cant do math

--- threeDBcWhyNot.js
+++ threeDBcWhyNot.js
@@ -46,12 +46,13 @@ class Body {
         this.sphere = new THREE.Mesh(new THREE.SphereGeometry(this.radius, 32, 32), new THREE.MeshPhongMaterial({color: 0xffffff}));
         this.sphere.position.set(this.pos.getXComp(), this.pos.getYComp(), this.pos.getZComp());
         scene.add(this.sphere);
+        this.id=this.sphere.id
         this.points = [];
         this.points.push( new THREE.Vector3( this.pos.getXComp(), this.pos.getYComp(), this.pos.getZComp() ) );
         this.geometry = new THREE.BufferGeometry().setFromPoints( this.points );
         this.material = new THREE.LineBasicMaterial({ color: 0xff00ff });
         this.line = new THREE.Line( this.geometry, this.material );
-        scene.add( this.line );
+        // scene.add( this.line );
         // this.path = new THREE.Line(new THREE.BufferGeometry().setFromPoints(new THREE.CatmullRomCurve3([new THREE.Vector3(this.pos.getXComp(), this.pos.getYComp(), this.pos.getZComp()),]).getPoints()), new THREE.LineBasicMaterial({ color: 0xff0000 }));
         // scene.add(this.path)
       }
@@ -65,8 +66,8 @@ class Body {
   
       this.pos.add(this.velocity)
 
-      this.points.push(new THREE.Vector3( this.pos.getXComp(), this.pos.getYComp(), this.pos.getZComp() ))
-      this.line.geometry = new THREE.BufferGeometry().setFromPoints( this.points );
+      // this.points.push(new THREE.Vector3( this.pos.getXComp(), this.pos.getYComp(), this.pos.getZComp() ))
+      // this.line.geometry = new THREE.BufferGeometry().setFromPoints( this.points );
  
 
     }
@@ -149,14 +150,20 @@ class Body {
     draw(){
         this.sphere.position.set(this.pos.getXComp(), this.pos.getYComp(), this.pos.getZComp());
     }
+    getRenderObject(){
+      return this.sphere
+    }
+    getId(){
+      return this.id
+    }
 
 }
 
 
 let scene = new THREE.Scene();
 let frameCount = 0;
-let frameDelay = 0;
-let camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
+let frameDelay = 10;
+let camera = new THREE.PerspectiveCamera(130, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 200;
 let al = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(al)
@@ -178,16 +185,34 @@ let delay = 0
 let objects = []
 let running = true
 
+let lastTime = 0
+window.addEventListener('wheel', function(event) {
+  let currTime = event.timeStamp
+  
+  vel = 1/(currTime-lastTime)*100
+  if (event.deltaY > 0) {
+    camera.position.z += Math.max(vel,1);
+  } else if (event.deltaY < 0) {
+    camera.position.z -= Math.max(vel,1);
+  }
+  lastTime=currTime
+
+});
+
+
+
+
+
 setup()
 main()
 
 function setup(){
-  objects.push(new Body(new Vector(-100,-100,100), new Vector(0, 1, 0),[new Vector(0,0,0)], 100,10,scene));
-  objects.push(new Body(new Vector(100,100,-100), new Vector(0, -1, 0),[new Vector(0,0,0)], 100,10,scene));
+  // objects.push(new Body(new Vector(-100,-100,100), new Vector(0, 1, 0),[new Vector(0,0,0)], 100,10,scene));
+  // objects.push(new Body(new Vector(100,100,-100), new Vector(0, -1, 0),[new Vector(0,0,0)], 100,10,scene));
   // objects.push(new Body(new Vector(500,0), new Vector(0, 0),[new Vector(0,0)], 10,9));
-//   for (let i = 0; i < 2 ; i++) {
-//       objects.push(new Body(new Vector(100, -200 + Math.random()*400, -200 + Math.random()*400), new Vector(0,0,0),[new Vector(0,0,0)], 10, 9, scene));
-//   }
+  for (let i = 0; i < 1000 ; i++) {
+      objects.push(new Body(new Vector(-200 + Math.random()*400, -200 + Math.random()*400, -200 + Math.random()*400), new Vector(0,0,0),[new Vector(0,0,0)], 1, 9, scene));
+  }
   // inner circle
   // let amount = 1000; 
   // let angleInc = (Math.PI * 2) / amount;
@@ -228,14 +253,14 @@ function pulleyForce(){
         let distance = object.getDistanceTo(object2)
         let distanceSquared = Math.pow(distance, 2)
         //i have decided that for this project index 0 will be reserved for the gravitational force
-        let mag = (G*object.getMass())/distanceSquared
+        let mag = (G*object2.getMass())/distanceSquared
         let xComp = object2Pos[0]-objectPos[0]
         let yComp = object2Pos[1]-objectPos[1]
         let zComp = object2Pos[2]-objectPos[2]
         let scale = mag/distance
         object.addAccelerationAtIndex(0, new Vector(xComp*scale,yComp*scale,zComp*scale))
 
-        mag = (G*object2.getMass())/distanceSquared
+        mag = (G*object.getMass())/distanceSquared
         scale = mag/distance
         object2.addAccelerationAtIndex(0, new Vector(-xComp*scale,-yComp*scale,-zComp*scale))
         
@@ -247,17 +272,45 @@ function pulleyForce(){
 
 function collisionDetection(){
   let stack = [...objects]
+  let newObjects = [...objects]
   while(stack.length>1){
     object = stack[0]
     for (object2 of stack){
       if (!object.equals(object2) && object.getDistanceTo(object2) <= object.getRadius() + object2.getRadius()){
-        
+
+        //im very sure i did some bad math here
+        let newRadius = Math.cbrt(Math.pow(object.getRadius(),3)+Math.pow(object2.getRadius(),3));
+        console.log(newRadius)
+        let pos = object.getPos()
+        let pos2 = object2.getPos()
+        let massOne = object.getMass()
+        let massTwo = object2.getMass()
+        let massSum = massOne+massTwo
+        let velOne = object.getVelocity()
+        let velTwo = object2.getVelocity()
+        let vx = 0*(massOne * velOne.getXComp() + massTwo * velTwo.getXComp())/(massSum)
+        let vy = 0*(massOne * velOne.getYComp() + massTwo * velTwo.getYComp())/(massSum)
+        let vz = 0*(massOne * velOne.getZComp() + massTwo * velTwo.getZComp())/(massSum)
+        newObjects.push(new Body(new Vector((pos[0]+pos2[0])/2,(pos[1]+pos2[1])/2,(pos[2]+pos2[2])/2), new Vector(vx, vy, vz), [new Vector(0,0,0)], massSum, newRadius, scene))
+
+        let renderOne = object.getId()
+        let renderTwo = object2.getId()
+        let index = scene.children.findIndex(obj => obj.id==renderOne);
+        scene.children.splice(index, 1);
+        index = scene.children.findIndex(obj => obj.id==renderTwo);
+        scene.children.splice(index, 1);
+
+        index = newObjects.findIndex(obj => obj.equals(object));
+        newObjects.splice(index, 1);
+        index = newObjects.findIndex(obj => obj.equals(object2));
+        newObjects.splice(index, 1);
+
 
       }
     }
     stack.splice(0,1)
   }
-
+  objects = newObjects
 }
 
 function updateObjects(){
@@ -272,8 +325,23 @@ function main(){
   frameCount++
   if (running && (frameDelay==0 || frameCount%frameDelay==0)){
     pulleyForce()
+    collisionDetection()
     updateObjects()
+    
+  
+    // let highestMass = -1;
+    // let objectWithHighestMass = null;
 
+    // for (let i = 0; i < objects.length; i++) {
+    //   if (objects[i].getMass() > highestMass) {
+    //     highestMass = objects[i].id;
+    //     objectWithHighestMass = objects[i];
+    //   }
+    // }
+    // let pos = objectWithHighestMass.getPos()
+    // camera.position.x = pos[0]
+    // camera.position.y = pos[1]
+    // camera.position.z = pos[2]+50
     renderer.render(scene, camera);
 
 
